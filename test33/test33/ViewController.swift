0984f720dc4baf91394172550c1ba19bf6ef55cb
Faran Ali <faran.ali.com>
2021-06-01 12:19:58 +0500

    Created project name test33

---
+++ test33/test33/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test33
+//
+//  Created by Faran Ali on 1.6.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
